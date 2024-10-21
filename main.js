31c92c9894359c5dfcd15928605db7c2f507d09a
svet-tech <svetla-gonchar@yandex.ru>
2024-10-21 17:57:46 +0300

    Add files via upload
    site about tourism

---
+++ main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper(".swiper", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    effect: "fade",
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
